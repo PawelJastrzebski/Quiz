711ffba3e9231109110f2e23c162eafdf9eeba0c
Paweł Jastrzębski <paweljastrzebski96@gmail.com>
2018-08-27 13:59:31 +0200

    initial commit

---
+++ src/main/java/Java9/com/Quiz/QuizApplication.java
@@ -0,0 +1,13 @@
+package Java9.com.Quiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizApplication {
+	// Start application
+	public static void main(String[] args) {
+		SpringApplication.run(QuizApplication.class, args);
+	}
+
+}
